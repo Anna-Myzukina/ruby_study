d7ac5ed2607f6cc3fbc1c7964bb7379b966a0234
Anna <anna.muzykina83@gmail.com>
2019-07-12 16:39:05 +0300

    Create Ruby Control Structures - Until.rb

---
+++ Ruby Control Structures - Until.rb
@@ -0,0 +1,27 @@
+=begin
+This challenge has a beautiful one-liner answer.
+
+"A hacker practices on HackerRank until getting to a rating of O(1) read as (Oh-one)"
+
+Call the method coder.practice until coder.oh_one? becomes true.
+
+Use the until control structure.
+
+until is the logical equivalent of while not.
+
+Hint
+
+while not <condition>
+    <code>
+end
+or
+
+until <condition>
+    <code>
+end
+or the beautiful one-liner
+
+<code> until <condition>  
+=end
+
+ coder.practice until coder.oh_one?
