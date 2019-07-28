becdfba710b813160d7e379bc09cc1e10ab804d2
Anna <anna.muzykina83@gmail.com>
2019-07-28 10:27:10 +0300

    Update Ruby Hash - Initialization.rb

--- Ruby Hash - Initialization.rb
+++ Ruby Hash - Initialization.rb
@@ -32,3 +32,7 @@ hackerrank["vivmbbs"] = 200
 
 =end
 
+# Initialize 3 variables here as explained in the problem statement
+empty_hash=Hash.new
+default_hash=Hash.new(1)
+hackerrank={"simmy"=>100,"vivmbbs"=>200}
